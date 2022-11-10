879cc13b74c473b2f97fcbc27507c4243da4364c
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2022-11-10 16:56:30 -0700

    Update neovim materials, specifically neogen
    Neogen handles docstring generation and I desired keymappings, so I
    implemented them

--- lua/neogen_rs.lua
+++ lua/neogen_rs.lua
@@ -2,3 +2,10 @@ require('neogen').setup({
     enabled = true,
     input_after_comment = true,
 })
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<Leader>df", ":lua require('neogen').generate()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>dc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>dt", ":lua require('neogen').generate({ type = 'type' })<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>de", ":lua require('neogen').generate({ type = 'file' })<CR>", opts)
+
