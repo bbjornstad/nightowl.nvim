2c2fe34448456d732eb42b8539f0ecd919ba5f57
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-07-24 11:56:00 -0600

    ui(colors): update colorscheme declarations and adjust highlighting
    In the long run, we want to make the highlighting capabilities agnostic over the
    choice of colorscheme, as this will allow us to keep thematic consistency for
    certain elements, namely telescope. For now though, we simply need to clean up
    some of the choices.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,73 @@
 local env = require("environment.ui")
 
-local function ursa_highlights(colors) end
-local linemapper = {
-  tundra = "tundra",
-  kanagawa = "kanagawa",
-  default = "auto",
-}
-
 if vim.fn.has("termguicolors") then
   vim.cmd([[set termguicolors]])
 end
 
+local function hlursa(overrides)
+  -- each item of overrides is a key-value pair where the keyname is the
+  -- highlight group and the value is a table with assignments in string name
+  -- form, along with an optional colordef_section key which is removed and is
+  -- used to pick the section of the "colors" table we need to parse out.
+  local function inner_hl(opts)
+    -- in this case that would be the opts table passed into this function
+    local hlink = opts.link_highlight or nil
+    -- this is either "theme" or "palette", and identifies the subsection of the
+    -- colorscheme that should be removed. When more colorschemes are accepted,
+    -- this is then going to be the union of all possible subdesignations.
+    local colordef_section = opts.colordef_section or "theme"
+    -- we need to toss these elements because of the fact that they are not
+    -- well-defined inputs for the final format, but we needed them to infer
+    -- behavior. moreover, there is impetus to do it this way to play nicely
+    -- with the vim.tbl_map function.
+    opts.link_highlight = nil
+    opts.colordef_section = nil
+
+    if hlink ~= nil then
+      return { link = hlink }
+    end
+  end
+
+  local function genfunc(colors, overrides)
+    return function(colors)
+      return vim.tbl_map(inner_hl, overrides)
+    end
+  end
+
+  return vim.tbl_map(inner_hl, overrides)
+end
+local hl_overrides = {
+  TelescopeTitle = { fg = "ui.special", bold = true },
+  TelescopePromptNormal = { bg = "ui.bg_p1" },
+
+  TelescopePromptBorder = { fg = "ui.bg_p1", bg = "ui.bg_p1" },
+  TelescopeResultsNormal = { fg = "ui.fg_dim", bg = "ui.bg_m1" },
+  TelescopeResultsBorder = { fg = "ui.bg_m1", bg = "ui.bg_m1" },
+  TelescopePreviewNormal = { bg = "ui.bg_dim" },
+  TelescopePreviewBorder = {
+    bg = "ui.bg_dim",
+    fg = "ui.bg_dim",
+  },
+  Pmenu = { fg = "ui.shade0", bg = "ui.bg_p1" },
+  PmenuSel = { fg = "NONE", bg = "ui.bg_p2" },
+  PmenuSbar = { bg = "ui.bg_m1" },
+  PmenuThumb = { bg = "ui.bg_p2" },
+  InclineNormal = { bg = "ui.bg_p1" },
+  InclineNormalNC = { bg = "ui.bg_m2" },
+  WinBar = { bg = "ui.bg_p1" },
+  WinBarNC = { bg = "ui.bg_p1" },
+  DropBarCurrentContext = { link = "TreesitterContext" },
+  DropBarMenuCurrentContext = { link = "TreesitterContext" },
+  DropBarIconCurrentContext = { link = "TreesitterContext" },
+  DropBarPreview = { link = "TreesitterContext" },
+}
+
 return {
   {
     "rebelot/kanagawa.nvim",
-    lazy = true,
+    lazy = false,
     priority = 1000,
     opts = {
-      --function(_, opts)
-      --  opts = vim.tbl_deep_extend("force", {
       globalStatus = true,
       dimInactive = true,
       commentStyle = { italic = true },
@@ -49,7 +98,13 @@ return {
           InclineNormal = { bg = colors.palette.sumiInk4 },
           InclineNormalNC = { bg = colors.palette.sumiInk1 },
           WinBar = { bg = colors.palette.sumiInk4 },
-          WinBarNC = { bg = colors.palette.sumiInk1 },
+          WinBarNC = { bg = colors.palette.sumiInk4 },
+          DropBarCurrentContext = { link = "TreesitterContext" },
+          DropBarMenuCurrentContext = { link = "TreesitterContext" },
+          DropBarIconCurrentContext = { link = "TreesitterContext" },
+          DropBarPreview = { link = "TreesitterContext" },
+          CustomContextVT = { fg = colors.palette.lightBlue, italic = true },
+          BiscuitColor = { fg = colors.palette.lightBlue, italic = true },
         }
       end,
     },
@@ -79,7 +134,7 @@ return {
         comments = { bold = false, italic = true },
         operators = { bold = true },
         types = { italic = true },
-      })
+      }, opts.syntax or {})
       opts.plugins = vim.tbl_extend("force", {
         lsp = true,
         treesitter = true,
@@ -92,13 +147,12 @@ return {
       opts.overwrite = vim.tbl_extend("force", {
         colors = {},
         highlights = {},
-      })
+      }, opts.overwrite or {})
     end,
   },
   { "dasupradyumna/midnight.nvim", lazy = true, priority = 994 },
   {
     "projekt0n/github-nvim-theme",
-    -- Dance would do me dirty every day like that so color me impressed.
     config = function()
       require("github-theme").setup({})
     end,
@@ -106,40 +160,61 @@ return {
     priority = 999,
   },
   {
-    "navarasu/onedark.nvim",
+    "olimorris/onedarkpro.nvim",
     config = function()
       -- require("onedark").load()
     end,
     lazy = true,
-    priority = 997,
+    priority = 990,
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({})
+    opts = function(_, opts)
+      local rppal = require("rose-pine.palette")
+      opts.variant = "auto" or opts.variant
+      opts.dark_variant = "main" or opts.dark_variant
+      opts.highlight_groups = vim.tbl_deep_extend("force", {
+        TelescopeTitle = { fg = rppal.text, bold = true },
+        TelescopePromptNormal = { bg = rppal.surface },
+        TelescopePromptBorder = { fg = rppal.base, bg = rppal.base },
+        TelescopeResultsNormal = { fg = rppal.text, bg = rppal.nc },
+        TelescopeResultsBorder = { fg = rppal.base, bg = rppal.base },
+        TelescopePreviewNormal = { bg = rppal.nc },
+        TelescopePreviewBorder = {
+          bg = rppal.base,
+          fg = rppal.base,
+        },
+        Pmenu = { fg = rppal.rose, bg = rppal.base },
+        PmenuSel = { fg = "NONE", bg = rppal.surface },
+        PmenuSbar = { bg = rppal.muted },
+        PmenuThumb = { bg = rppal.overlay },
+        InclineNormal = { bg = rppal.surface },
+        InclineNormalNC = { bg = rppal.base },
+        WinBar = { bg = rppal.surface },
+        WinBarNC = { bg = rppal.base },
+      }, opts.highlight_groups or {})
     end,
+    config = true,
     lazy = true,
     priority = 996,
-  },
-  {
-    "NLKNguyen/papercolor-theme",
-    priority = 998,
-    lazy = true,
-  },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = true,
-    priority = 995,
-    config = function()
-      -- require("nordic").load()
-    end,
   },
   {
     "sainnhe/everforest",
     lazy = true,
     priority = 992,
-    config = function() end,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_better_performance = 1
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
+  {
+    "yorik1984/newpaper.nvim",
+    config = true,
+    opts = {
+      style = "light",
+    },
   },
   { "LazyVim/LazyVim", opts = { colorscheme = env.default_colorscheme } },
 }
