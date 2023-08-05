af38b033fdb47460a696d5cb23eb8afbe68c890f
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-08-05 12:33:00 -0600

    restruct(scope): update telescope configurations to match recent changes
    This is pretty simple and self-explanatory, lots of changes have been made and
    telescope needs to be updated to match.

--- lua/plugins/scope.lua
+++ lua/plugins/scope.lua
@@ -153,7 +153,7 @@ return {
       },
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release; cmake --build build --config Release; cmake --install build --prefix build",
         dependencies = { "junegunn/fzf", "junegunn/fzf.vim" },
         config = function()
           require("telescope").load_extension("fzf")
