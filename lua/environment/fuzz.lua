c4acb3d87670a2b292673a06b09f55c621dc211f
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-08-10 02:38:57 -0600

    clean(env): clean environment configuration scripts to reflect recent changes
    there were some vestigial files in the environment directory that have now been
    removed.

---
+++ lua/environment/fuzz.lua
@@ -0,0 +1,51 @@
+local env = {}
+
+--- selects a possible profile option from the selection of fzf-lua profiles
+--- that are available here; one of the following options:
+--- * "default",
+--- * "fzf-native"
+--- * "fzf-tmux" -- requires tmux
+--- * "max-perf" -- generally not necessary on most setups
+--- * "telescope" -- tries to match telescope
+--- * "skim" -- ideally this is preferred but I'm currently having a failed skim
+---   install for some reason.
+env.fzf_profile = "default"
+env.fzf_border = "thicc"
+env.fullscreen = false
+
+env.preview = {
+  -- default     = 'bat',           -- override the default previewer?
+  -- default uses the 'builtin' previewer
+  border = "border", -- border|noborder, applies only to
+  -- native fzf previewers (bat/cat/git/etc)
+  wrap = "nowrap", -- wrap|nowrap
+  hidden = "nohidden", -- hidden|nohidden
+  vertical = "down:45%", -- up|down:size
+  horizontal = "right:60%", -- right|left:size
+  layout = "flex", -- horizontal|vertical|flex
+  flip_columns = 120, -- #cols to switch to horizontal on flex
+  -- Only used with the builtin previewer:
+  title = true, -- preview border title (file/buf)?
+  title_pos = "right", -- left|center|right, title alignment
+  scrollbar = "border", -- `false` or string:'float|border'
+  -- float:  in-window floating border
+  -- border: in-border chars (see below)
+  scrolloff = "-2", -- float scrollbar offset from right
+  -- applies only when scrollbar = 'float'
+  scrollchars = { "█", "" }, -- scrollbar chars ({ <full>, <empty> }
+  -- applies only when scrollbar = 'border'
+  delay = 100, -- delay(ms) displaying the preview
+  -- prevents lag on fast scrolling
+  winopts = { -- builtin previewer window options
+    number = true,
+    relativenumber = false,
+    cursorline = true,
+    cursorlineopt = "both",
+    cursorcolumn = false,
+    signcolumn = "yes",
+    list = false,
+    foldenable = false,
+    foldmethod = "manual",
+  },
+}
+return env
--- lua/environment/keys.lua
+++ lua/environment/keys.lua
@@ -1,7 +1,5 @@
 local mod = {}
 
-local aistem = ";"
-
 function mod.map(modes)
   local function returnable(lhs, rhs, opts)
     vim.keymap.set(modes, lhs, rhs, opts)
@@ -10,65 +8,110 @@ function mod.map(modes)
   return returnable
 end
 
+function mod.wk_family_inject(desc, keys, opts)
+  opts = opts or {}
+  if type(keys) ~= "table" then
+    keys = { keys }
+  end
+  local final = {}
+
+  for _, key in pairs(keys) do
+    final[key] = "+" .. desc
+  end
+  return {
+    "folke/which-key.nvim",
+    opts = vim.tbl_deep_extend("force", {
+      defaults = final,
+    }, opts),
+  }
+end
+
+--- set up a table that will hold all of the keystem selections for access in
+--- other files. this serves as a master record.
 mod.stems = {}
-mod.stems.ccc = "<leader>uh"
-mod.stems.toggleterm = "<leader>t"
-mod.stems.easyread = "<leader>uB"
-mod.stems.rest = "<leader>R"
-mod.stems.lsp = "<leader>c"
-mod.stems.lazy = "<leader>L"
+
+--- the stems.base item holds the baseline keystems for the various groups of
+--- keybindings that are made using nightowl.nvim
+mod.stems.base = {}
+
+mod.stems.base.core = "<leader>"
+mod.stems.base.editor = "<localleader>"
+
+--- assignment of keystems for each group into the base.
+mod.stems.base.ai = ";"
+mod.stems.base.ui = mod.stems.base.core .. "u"
+mod.stems.base.remote = mod.stems.base.core .. "r"
+mod.stems.base.telescope = mod.stems.base.core .. "<leader>"
+mod.stems.base.code = mod.stems.base.core .. "c"
+mod.stems.base.tasks = "<bar>"
+mod.stems.base.repl = "<F2>"
+mod.stems.base.fuzzy = "Z"
+mod.stems.base.buffers = "q"
+
+mod.stems.ccc = mod.stems.base.core .. "uh"
+mod.stems.toggleterm = mod.stems.base.core .. "t"
+mod.stems.easyread = mod.stems.base.core .. "uB"
+mod.stems.rest = mod.stems.base.core .. "R"
+mod.stems.lsp = mod.stems.base.core .. "c"
+mod.stems.lazy = mod.stems.base.core .. "L"
 mod.stems.cmp = "<C-o>"
 
-mod.stems.taskorg = "<bar>"
-mod.stems.pomodoro = mod.stems.taskorg .. "p"
-mod.stems.overseer = mod.stems.taskorg .. "v"
-mod.stems.unfog = mod.stems.taskorg .. "u"
-mod.stems._do = mod.stems.taskorg .. "d"
+mod.stems.pomodoro = mod.stems.base.tasks .. "p"
+mod.stems.overseer = mod.stems.base.tasks .. "v"
+mod.stems.unfog = mod.stems.base.tasks .. "u"
+mod.stems._do = mod.stems.base.tasks .. "d"
+mod.stems.conduct = mod.stems.base.tasks .. "c"
+
+mod.stems.remote = mod.stems.base.core .. "r"
 
 --------------------------------------------------------------------------------
-mod.stems.BASEAI = aistem
-mod.stems.neural = aistem .. "n"
-mod.stems.copilot = aistem .. "g"
-mod.stems.codeium = aistem .. "d"
-mod.stems.neoai = aistem .. "e"
-mod.stems.cmp_ai = aistem .. "a"
-mod.stems.hfcc = aistem .. "h"
-mod.stems.chatgpt = aistem .. "c"
-mod.stems.codegpt = aistem .. "o"
-mod.stems.rgpt = aistem .. "r"
-mod.stems.navi = aistem .. "v"
-mod.stems.explain_it = aistem .. "x"
-mod.stems.tabnine = aistem .. "9"
+mod.stems.neural = mod.stems.base.ai .. "n"
+mod.stems.copilot = mod.stems.base.ai .. "g"
+mod.stems.codeium = mod.stems.base.ai .. "d"
+mod.stems.neoai = mod.stems.base.ai .. "e"
+mod.stems.cmp_ai = mod.stems.base.ai .. "a"
+mod.stems.hfcc = mod.stems.base.ai .. "h"
+mod.stems.chatgpt = mod.stems.base.ai .. "c"
+mod.stems.codegpt = mod.stems.base.ai .. "o"
+mod.stems.rgpt = mod.stems.base.ai .. "r"
+mod.stems.navi = mod.stems.base.ai .. "v"
+mod.stems.explain_it = mod.stems.base.ai .. "x"
+mod.stems.tabnine = mod.stems.base.ai .. "9"
+mod.stems.doctor = mod.stems.base.ai .. "d"
+mod.stems.llm = mod.stems.base.ai .. "l"
 
-mod.stems.repl = "`"
-mod.stems.sniprun = mod.stems.repl .. "s"
-mod.stems.iron = mod.stems.repl .. "r"
-mod.stems.vlime = mod.stems.repl .. "v"
-mod.stems.acid = mod.stems.repl .. "a"
-mod.stems.conjure = mod.stems.repl .. "c"
-mod.stems.jupyter = mod.stems.repl .. "j"
+mod.stems.sniprun = mod.stems.base.repl .. "s"
+mod.stems.iron = mod.stems.base.repl .. "r"
+mod.stems.vlime = mod.stems.base.repl .. "v"
+mod.stems.acid = mod.stems.base.repl .. "a"
+mod.stems.conjure = mod.stems.base.repl .. "c"
+mod.stems.jupyter = mod.stems.base.repl .. "j"
 
-mod.stems.telescope = "<leader><leader>"
-mod.stems.glow = "<leader>P"
-mod.stems.notify = "<leader>n"
-mod.stems.vista = "<leader>v"
-mod.stems.neogen = "<leader>D"
-mod.stems.lens = "<leader>uo"
-mod.stems.iron = "<leader>r"
-mod.stems.oil = "<leader>f"
-mod.stems.git = "<leader>g"
-mod.stems.block = "<leader>ub"
-mod.stems.tterm = "<leader>T"
-mod.stems.navbuddy = "<leader>v"
-mod.stems.undotree = "<leader>U"
-mod.stems.based = "<leader>B"
+mod.stems.telescope = mod.stems.base.telescope
+mod.stems.glow = mod.stems.base.core .. "P"
+mod.stems.notify = mod.stems.base.core .. "N"
+mod.stems.vista = mod.stems.base.core .. "v"
+mod.stems.neogen = mod.stems.base.core .. "D"
+mod.stems.lens = mod.stems.base.core .. "uo"
+mod.stems.iron = mod.stems.base.core .. "r"
+mod.stems.oil = mod.stems.base.core .. "f"
+mod.stems.git = mod.stems.base.core .. "g"
+mod.stems.block = mod.stems.base.core .. "ub"
+mod.stems.tterm = mod.stems.base.core .. "T"
+mod.stems.navbuddy = mod.stems.base.core .. "v"
+mod.stems.undotree = mod.stems.base.core .. "U"
+mod.stems.based = mod.stems.base.core .. "B"
+mod.stems.notice = mod.stems.base.core .. "N"
+mod.stems.nnn = mod.stems.base.core .. "nn"
+mod.stems.treesj = "gj"
+mod.stems.files = mod.stems.base.core .. "f"
 
-mod.stems.figlet = "<localleader>i"
-mod.stems.figban = "<localleader>f"
-mod.stems.textgen = "<localleader>t"
-mod.stems.cbox = "<localleader>b"
-mod.stems.cline = "<localleader>l"
+mod.stems.figlet = mod.stems.base.editor .. "i"
+mod.stems.figban = mod.stems.base.editor .. "f"
+mod.stems.textgen = mod.stems.base.editor .. "t"
+mod.stems.cbox = mod.stems.base.editor .. "b"
+mod.stems.cline = mod.stems.base.editor .. "l"
 
-mod.stems.modeline = "<localleader>m"
+mod.stems.modeline = mod.stems.base.editor .. "m"
 
 return mod
