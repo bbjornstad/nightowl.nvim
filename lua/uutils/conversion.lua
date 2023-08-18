afbe7e69615c6a7bc95797a67a21cff842892c50
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-08-18 14:06:52 -0600

    fix(utils): move conversion implementations to a non-pluralized module name

---
+++ lua/uutils/conversion.lua
@@ -0,0 +1,34 @@
+local mod = {}
+
+function mod.from_env(varname, caster)
+  caster = caster or function(...)
+    return ...
+  end
+
+  local res = os.getenv(varname)
+  return res
+end
+
+function mod.bool_from_env(varname)
+  return mod.from_env(varname, function(val)
+    if val == "true" then
+      return true
+    end
+    return false
+  end)
+end
+
+function mod.toboolean(v)
+  local ok, res = pcall(tonumber, v)
+  if ok then
+    return res
+  end
+  if v == "false" or not v then
+    return false
+  elseif v == "true" then
+    return true
+  end
+  return nil
+end
+
+return mod
