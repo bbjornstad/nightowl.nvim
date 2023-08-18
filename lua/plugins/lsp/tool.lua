95af5447ca22a9d1d28a3489bce6ebd4b70b598a
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-08-18 14:03:51 -0600

    update(lsp): adjust plugin load order specifications at lsp level
    This is in search of a fix to the elusive problem which is the notification on
    startup that neoconf should be run before any lspconfig setup. Previously, it
    seems as though there was some kind of circular dependency introduced during the
    core lsp initialization due to lsp-zero being included superfluously in
    dependency fields of certain lsp plugins.

--- lua/plugins/lsp/tool.lua
+++ lua/plugins/lsp/tool.lua
@@ -1,6 +1,7 @@
 local keystems = require("environment.keys").stems
 local env = require("environment.ui")
 local key_vista = keystems.vista
+local key_aerial = key_vista .. "a"
 local key_lens = keystems.lens
 local key_ui = keystems.base.ui
 
@@ -26,148 +27,148 @@ return {
       },
     },
   },
-  -- {
-  --   "stevearc/aerial.nvim",
-  --   opts = {
-  --     layout = {
-  --       max_width = { 36, 0.2 },
-  --       min_width = 12,
-  --     },
-  --     open_automatic = true,
-  --     keymaps = {
-  --       ["?"] = "actions.show_help",
-  --       ["gh"] = "actions.show_help",
-  --       ["<CR>"] = "actions.jump",
-  --       ["<2-LeftMouse>"] = "actions.jump",
-  --       ["<C-s>"] = "actions.jump_vsplit",
-  --       ["<C-h>"] = "actions.jump_split",
-  --       ["p"] = "actions.scroll",
-  --       ["<C-d>"] = "actions.down_and_scroll",
-  --       ["<C-u>"] = "actions.up_and_scroll",
-  --       ["{"] = "actions.prev",
-  --       ["}"] = "actions.next",
-  --       ["[["] = "actions.prev_up",
-  --       ["]]"] = "actions.next_up",
-  --       ["Q"] = "actions.close",
-  --       ["o"] = "actions.tree_toggle",
-  --       ["za"] = "actions.tree_toggle",
-  --       ["O"] = "actions.tree_toggle_recursive",
-  --       ["zA"] = "actions.tree_toggle_recursive",
-  --       ["l"] = "actions.tree_open",
-  --       ["zo"] = "actions.tree_open",
-  --       ["L"] = "actions.tree_open_recursive",
-  --       ["zO"] = "actions.tree_open_recursive",
-  --       ["h"] = "actions.tree_close",
-  --       ["zc"] = "actions.tree_close",
-  --       ["H"] = "actions.tree_close_recursive",
-  --       ["zC"] = "actions.tree_close_recursive",
-  --       ["zr"] = "actions.tree_increase_fold_level",
-  --       ["zR"] = "actions.tree_open_all",
-  --       ["zm"] = "actions.tree_decrease_fold_level",
-  --       ["zM"] = "actions.tree_close_all",
-  --       ["zx"] = "actions.tree_sync_folds",
-  --       ["zX"] = "actions.tree_sync_folds",
-  --     },
-  --     filter_kind = false,
-  --     show_guides = true,
-  --     guides = {
-  --       mid_item = "╞",
-  --       last_item = "╘	",
-  --       nested_top = "│",
-  --       whitespace = "═",
-  --     },
-  --     highlight_on_hover = true,
-  --     highlight_on_jump = 800,
-  --     autojump = true,
-  --     ignore = {
-  --       filetypes = env.ft_ignore_list,
-  --     },
-  --     float = {
-  --       border = env.borders.main,
-  --       relative = "cursor",
-  --       max_height = 0.8,
-  --       min_height = { 12, 0.1 },
-  --     },
-  --     nav = {
-  --       border = env.borders.main,
-  --       max_height = 0.8,
-  --       min_height = { 12, 0.1 },
-  --       max_width = 0.6,
-  --       min_width = { 0.2, 24 },
-  --       autojump = true,
-  --       preview = true,
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("aerial").setup(opts)
-  --   end,
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "nvim-tree/nvim-web-devicons",
-  --   },
-  --   cmd = {
-  --     "AerialToggle",
-  --     "AerialOpen",
-  --     "AerialOpenAll",
-  --     "AerialInfo",
-  --     "AerialNavToggle",
-  --     "AerialNavOpen",
-  --   },
-  --   keys = {
-  --     {
-  --       key_vista .. "a",
-  --       "<CMD>AerialToggle<CR>",
-  --       mode = "n",
-  --       desc = "vista=> toggle symbols outline",
-  --     },
-  --     {
-  --       key_vista .. "q",
-  --       "<CMD>AerialClose<CR>",
-  --       mode = "n",
-  --       desc = "vista=> close symbols outline",
-  --     },
-  --     {
-  --       key_vista .. "o",
-  --       "<CMD>AerialOpen<CR>",
-  --       mode = "n",
-  --       desc = "vista=> open symbols outline",
-  --     },
-  --     {
-  --       key_vista .. "A",
-  --       "<CMD>AerialToggle<CR>",
-  --       mode = "n",
-  --       desc = "vista=> toggle[!] symbols outline",
-  --     },
-  --     {
-  --       key_vista .. "Q",
-  --       "<CMD>AerialClose<CR>",
-  --       mode = "n",
-  --       desc = "vista=> close[!] symbols outline",
-  --     },
-  --     {
-  --       key_vista .. "O",
-  --       "<CMD>AerialOpen<CR>",
-  --       mode = "n",
-  --       desc = "vista=> open[!] symbols outline",
-  --     },
-  --   },
-  -- },
+  {
+    "stevearc/aerial.nvim",
+    event = "LspAttach",
+    opts = {
+      layout = {
+        max_width = { 36, 0.2 },
+        min_width = 12,
+      },
+      open_automatic = false,
+      keymaps = {
+        ["?"] = "actions.show_help",
+        ["gh"] = "actions.show_help",
+        ["<CR>"] = "actions.jump",
+        ["<2-LeftMouse>"] = "actions.jump",
+        ["<C-v>"] = "actions.jump_vsplit",
+        ["<C-s>"] = "actions.jump_split",
+        ["p"] = "actions.scroll",
+        ["<C-d>"] = "actions.down_and_scroll",
+        ["<C-u>"] = "actions.up_and_scroll",
+        ["{"] = "actions.prev",
+        ["}"] = "actions.next",
+        ["[["] = "actions.prev_up",
+        ["]]"] = "actions.next_up",
+        ["Q"] = "actions.close",
+        ["o"] = "actions.tree_toggle",
+        ["za"] = "actions.tree_toggle",
+        ["O"] = "actions.tree_toggle_recursive",
+        ["zA"] = "actions.tree_toggle_recursive",
+        ["l"] = "actions.tree_open",
+        ["zo"] = "actions.tree_open",
+        ["L"] = "actions.tree_open_recursive",
+        ["zO"] = "actions.tree_open_recursive",
+        ["h"] = "actions.tree_close",
+        ["zc"] = "actions.tree_close",
+        ["H"] = "actions.tree_close_recursive",
+        ["zC"] = "actions.tree_close_recursive",
+        ["zr"] = "actions.tree_increase_fold_level",
+        ["zR"] = "actions.tree_open_all",
+        ["zm"] = "actions.tree_decrease_fold_level",
+        ["zM"] = "actions.tree_close_all",
+        ["zx"] = "actions.tree_sync_folds",
+        ["zX"] = "actions.tree_sync_folds",
+      },
+      filter_kind = false,
+      show_guides = true,
+      guides = {
+        mid_item = "╞",
+        last_item = "╘	",
+        nested_top = "│",
+        whitespace = " ",
+      },
+      highlight_on_hover = true,
+      highlight_on_jump = 800,
+      autojump = true,
+      ignore = {
+        filetypes = env.ft_ignore_list,
+      },
+      float = {
+        border = env.borders.main,
+        relative = "cursor",
+        max_height = 0.8,
+        min_height = { 12, 0.1 },
+      },
+      nav = {
+        border = env.borders.main,
+        max_height = 0.8,
+        min_height = { 12, 0.1 },
+        max_width = 0.6,
+        min_width = { 0.2, 24 },
+        autojump = true,
+        preview = true,
+      },
+    },
+    config = function(_, opts)
+      require("aerial").setup(opts)
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    cmd = {
+      "AerialToggle",
+      "AerialOpen",
+      "AerialOpenAll",
+      "AerialInfo",
+      "AerialNavToggle",
+      "AerialNavOpen",
+    },
+    keys = {
+      {
+        key_aerial .. "a",
+        "<CMD>AerialToggle<CR>",
+        mode = "n",
+        desc = "vista=> toggle symbols outline",
+      },
+      {
+        key_aerial .. "q",
+        "<CMD>AerialClose<CR>",
+        mode = "n",
+        desc = "vista=> close symbols outline",
+      },
+      {
+        key_aerial .. "o",
+        "<CMD>AerialOpen<CR>",
+        mode = "n",
+        desc = "vista=> open symbols outline",
+      },
+      {
+        key_aerial .. "A",
+        "<CMD>AerialToggle<CR>",
+        mode = "n",
+        desc = "vista=> toggle[!] symbols outline",
+      },
+      {
+        key_aerial .. "Q",
+        "<CMD>AerialClose<CR>",
+        mode = "n",
+        desc = "vista=> close[!] symbols outline",
+      },
+      {
+        key_aerial .. "O",
+        "<CMD>AerialOpen<CR>",
+        mode = "n",
+        desc = "vista=> open[!] symbols outline",
+      },
+    },
+  },
   {
     "simrat39/symbols-outline.nvim",
+    event = "LspAttach",
     cmd = { "SymbolsOutline", "SymbolsOutlineOpen", "SymbolsOutlineClose" },
     opts = {
       highlight_hovered_item = true,
       show_guides = true,
       show_numbers = false,
       show_relative_numbers = false,
-      position = "left",
-      width = 36,
       auto_close = false,
       auto_preview = false,
-      winblend = 15,
+      winblend = 20,
       keymaps = {
         code_actions = { "a", "ga", "<leader>ca" },
-        close = { "<Esc>", "Q" },
+        close = { "Q" },
         toggle_preview = "<C-p>",
         hover_symbol = "K",
         rename_symbol = "r",
@@ -177,9 +178,6 @@ return {
         fold_all = "zM",
         unfold_all = "zR",
         fold_reset = "zW",
-      },
-      symbols = {
-        event = { icon = "", hl = "@type" },
       },
     },
     config = true,
@@ -265,32 +263,18 @@ return {
       },
     },
   },
-  -- {
-  --   "code-biscuits/nvim-biscuits",
-  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  --   opts = {
-  --     cursor_line_only = false,
-  --     toggle_keybind = key_ui .. "u",
-  --     show_on_start = true,
-  --     prefix_string = " 󰁭 󰟵 󰙔  ",
-  --     language_config = {
-  --       vimdoc = {
-  --         disabled = true,
-  --       },
-  --       help = {
-  --         disabled = true,
-  --       },
-  --     },
-  --   },
-  --   event = { "BufWinEnter" },
-  --   build = ":TSUpdate",
-  -- },
   {
     "utilyre/sentiment.nvim",
     version = "*",
-    event = "VeryLazy", -- keep for lazy loading
+    event = "LspAttach", -- keep for lazy loading
     opts = {
-      -- config
+      pairs = {
+        { "<CMD>", "<CR>" },
+        { "(", ")" },
+        { "{", "}" },
+        { "[", "]" },
+        { "<", ">" },
+      },
     },
     init = function()
       -- `matchparen.vim` needs to be disabled manually in case of lazy loading
@@ -304,30 +288,49 @@ return {
   {
     "m-demare/hlargs.nvim",
     opts = {
-      highlight = {},
-      excluded_filetypes = {},
+      highlight = {
+        -- Hlargs = { link = "@include" },
+      },
+      excluded_filetypes = env.ft_ignore_list,
+      extras = { named_parameters = true },
     },
     config = true,
     event = "LspAttach",
   },
   {
-    "mizlan/iswap.nvim",
-    opts = {},
-    config = true,
-    keys = {},
-    cmd = {
-      "ISwap",
-      "ISwapWith",
-      "ISwapNode",
-      "ISwapNodeWith",
-      "ISwapNodeWithLeft",
-      "ISwapNodeWithRight",
-      "IMove",
-      "IMoveWith",
-      "IMoveNode",
-      "IMoveNodeWith",
-      "IMoveNodeWithLeft",
-      "IMoveNodeWithRight",
+    "echasnovski/mini.operators",
+    version = false,
+    opts = {
+      evaluate = {
+        prefix = "g=",
+      },
+      exchange = {
+        prefix = "gx",
+      },
+      multiply = {
+        prefix = "gm",
+      },
+      replace = {
+        prefix = "gR",
+      },
+      sort = {
+        prefix = "gs",
+      },
     },
   },
+  --   {
+  --     "ivanjermakov/troublesum.nvim",
+  --     event = "LspAttach",
+  --     opts = {
+  --       enabled = true,
+  --       autocmd = true,
+  --       severity_format = { "", "", "", "󱍅" },
+  --       format = function(counts) end,
+  --       display_summary = function(bufnr, ns, text) end,
+  --     },
+  --     config = function(_, opts)
+  --       -- require("troublesum").override_config(opts)
+  --       require("troublesum").setup(opts)
+  --     end,
+  --   },
 }
