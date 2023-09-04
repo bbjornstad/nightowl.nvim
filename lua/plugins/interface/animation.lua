1bf0b3883d0520dc0334abb6f934f22a9052c85e
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-09-04 16:11:52 -0600

    feat(cursor): add SmoothCursor for visually improved cursor experience
    SmoothCursor adds a cursor guide on the gutter and generally improves
    cursor experience

--- lua/plugins/interface/animation.lua
+++ lua/plugins/interface/animation.lua
@@ -1,3 +1,5 @@
+local sound_dir = vim.fn.expand("config") .. "/interface_sounds"
+
 return {
   {
     "declancm/cinnamon.nvim",
@@ -16,5 +18,59 @@ return {
       horizontal_scroll = true,
     },
     event = "VeryLazy",
+  },
+  {
+    "gen740/SmoothCursor.nvim",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("smoothcursor").setup(opts)
+      local autocmd = vim.api.nvim_create_autocmd
+
+      autocmd({ "ModeChanged" }, {
+        callback = function()
+          local current_mode = vim.fn.mode()
+          if current_mode == "n" then
+            vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#8aa872" })
+            vim.fn.sign_define("smoothcursor", { text = "" })
+          elseif current_mode == "v" then
+            vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#bf616a" })
+            vim.fn.sign_define("smoothcursor", { text = "" })
+          elseif current_mode == "V" then
+            vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#bf616a" })
+            vim.fn.sign_define("smoothcursor", { text = "" })
+          elseif current_mode == "�" then
+            vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#bf616a" })
+            vim.fn.sign_define("smoothcursor", { text = "" })
+          elseif current_mode == "i" then
+            vim.api.nvim_set_hl(0, "SmoothCursor", { fg = "#668aab" })
+            vim.fn.sign_define("smoothcursor", { text = "" })
+          end
+        end,
+      })
+    end,
+    opts = {
+      autostart = true,
+      line_hl = "CursorLine",
+      speed = 36,
+      threshold = 3,
+      fancy = {
+        enable = true,
+        head = { cursor = "󰵵" },
+        body = {},
+      },
+    },
+  },
+  {
+    "whleucka/reverb.nvim",
+    event = "BufReadPre",
+    opts = {
+      sounds = {
+        BufRead = vim.fs.joinpath(sound_dir, "start.ogg"),
+        CursorMovedI = vim.fs.joinpath(sound_dir, "click.ogg"),
+        InsertLeave = vim.fs.joinpath(sound_dir, "toggle.ogg"),
+        ExitPre = vim.fs.joinpath(sound_dir, "exit.ogg"),
+        BufWrite = vim.fs.joinpath(sound_dir, "save.ogg"),
+      },
+    },
   },
 }
