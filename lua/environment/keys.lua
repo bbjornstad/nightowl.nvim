1dfe45a2409f1eb782952fce50f42809dd3b82be
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-09-04 16:07:37 -0600

    fix(keybind): fix the keybind stems to accomodate some recent changes

--- lua/environment/keys.lua
+++ lua/environment/keys.lua
@@ -8,24 +8,6 @@ function mod.map(modes)
   return returnable
 end
 
-function mod.wk_family_inject(desc, keys, opts)
-  opts = opts or {}
-  if type(keys) ~= "table" then
-    keys = { keys }
-  end
-  local final = {}
-
-  for _, key in pairs(keys) do
-    final[key] = "+" .. desc
-  end
-  return {
-    "folke/which-key.nvim",
-    opts = vim.tbl_deep_extend("force", {
-      defaults = final,
-    }, opts),
-  }
-end
-
 --- set up a table that will hold all of the keystem selections for access in
 --- other files. this serves as a master record.
 mod.stems = {}
@@ -54,13 +36,14 @@ mod.stems.base.portal = "go"
 mod.stems.base.neorg = "'"
 mod.stems.base.lsp = "gl"
 mod.stems.base.godocs = "gH"
+mod.stems.base.files = mod.stems.base.core .. "f"
 
 mod.stems.ccc = mod.stems.base.core .. "uk"
 mod.stems.easyread = mod.stems.base.core .. "uB"
 mod.stems.rest = mod.stems.base.core .. "R"
 mod.stems.lsp = mod.stems.base.core .. "c"
 mod.stems.lazy = mod.stems.base.core .. "L"
-mod.stems.cmp = "<C-x>"
+mod.stems.cmp = "<C-o>"
 
 mod.stems.pomodoro = mod.stems.base.tasks .. "p"
 mod.stems.overseer = mod.stems.base.tasks .. "v"
@@ -72,6 +55,8 @@ mod.stems.memento = mod.stems.base.tasks .. "m"
 mod.stems.remote = mod.stems.base.core .. "r"
 mod.stems.regex = mod.stems.base.core .. "R"
 
+--------------------------------------------------------------------------------
+-- AI Tooling...................................................................
 --------------------------------------------------------------------------------
 mod.stems.neural = mod.stems.base.ai .. "n"
 mod.stems.copilot = mod.stems.base.ai .. "g"
@@ -90,6 +75,18 @@ mod.stems.llm = mod.stems.base.ai .. "l"
 mod.stems.wtf = mod.stems.base.ai .. "w"
 mod.stems.backseat = mod.stems.base.ai .. "b"
 
+--------------------------------------------------------------------------------
+-- file managers................................................................
+--------------------------------------------------------------------------------
+mod.stems.files = mod.stems.base.core .. "f"
+mod.stems.oil = mod.stems.base.core .. "f"
+mod.stems.traveller = mod.stems.base.files .. "t"
+mod.stems.nnn = mod.stems.base.core .. "nn"
+mod.stems.broot = mod.stems.base.core .. "fe"
+
+--------------------------------------------------------------------------------
+-- repl types...................................................................
+--------------------------------------------------------------------------------
 mod.stems.sniprun = mod.stems.base.repl .. "s"
 mod.stems.iron = mod.stems.base.repl .. "r"
 mod.stems.vlime = mod.stems.base.repl .. "v"
@@ -102,26 +99,20 @@ mod.stems.glow = mod.stems.base.core .. "P"
 mod.stems.notify = mod.stems.base.core .. "N"
 mod.stems.vista = mod.stems.base.core .. "v"
 mod.stems.neogen = mod.stems.base.core .. "D"
-mod.stems.lens = mod.stems.base.core .. "uo"
+mod.stems.lens = mod.stems.base.ui .. "o"
 mod.stems.iron = mod.stems.base.core .. "r"
-mod.stems.oil = mod.stems.base.core .. "f"
 mod.stems.git = mod.stems.base.core .. "g"
-mod.stems.block = mod.stems.base.core .. "ub"
+mod.stems.block = mod.stems.base.ui .. "b"
 mod.stems.toggleterm = mod.stems.base.core .. "T"
 mod.stems.customterm = mod.stems.base.core .. "m"
 mod.stems.navbuddy = mod.stems.base.core .. "v"
 mod.stems.undotree = mod.stems.base.core .. "U"
 mod.stems.based = mod.stems.base.core .. "B"
 mod.stems.notice = mod.stems.base.core .. "N"
-mod.stems.nnn = mod.stems.base.core .. "nn"
-mod.stems.broot = mod.stems.base.core .. "fe"
-mod.stems.focus = mod.stems.base.core .. "uW"
+mod.stems.focus = mod.stems.base.ui .. "W"
 mod.stems.devdocs = mod.stems.base.godocs
 
-mod.stems.htop = mod.stems.base.core .. "mh"
-
 mod.stems.treesj = "gj"
-mod.stems.files = mod.stems.base.core .. "f"
 mod.stems.attempt = mod.stems.base.core .. "a"
 
 mod.stems.figlet = mod.stems.base.editor .. "i"
