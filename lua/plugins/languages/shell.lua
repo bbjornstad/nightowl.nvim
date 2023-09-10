6bc1b22581ab833ceab7a6b908e8cbdc8082fa71
Bailey Bjornstad <bailey.bjornstad@fastmail.com>
2023-09-10 13:12:27 -0600

    fix(nu): add the correct filetype detection for nushell files

--- lua/plugins/languages/shell.lua
+++ lua/plugins/languages/shell.lua
@@ -9,6 +9,31 @@ return {
       use_lsp_features = true,
       all_cmd_names = [[nu -c 'help commands | get name | str join "\n"']],
     },
+    init = function()
+      vim.filetype.add({
+        extension = {
+          nu = "nu",
+        },
+      })
+    end,
     config = true,
   },
+  -- {
+  --   "zioroboco/nu-ls.nvim",
+  --   ft = { "nu" },
+  --   config = function(_, opts)
+  --     require("nu-ls").setup(opts)
+  --   end,
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     {
+  --       "jose-elias-alvarez/null-ls.nvim",
+  --       opts = {
+  --         sources = {
+  --           require("nu-ls"),
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
