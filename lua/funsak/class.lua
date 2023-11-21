9ce3091612f867e23855cee7f562dd69e9317cc0
Bailey Bjornstad <bailey@bjornstad.dev>
2023-11-21 03:14:46 -0700

    feat(class): introduce a standard class implementation to funsak
    This is simply a way to consistently treat the need to store certain
    functions alongside corresponding data across all modules of the
    configuration.

---
+++ lua/funsak/class.lua
@@ -0,0 +1,75 @@
+---@module "funsak.class" extensible definition of a standard-style class for
+---holding data and methods.
+---@author Bailey Bjornstad | ursa-major [Via https://github.com/rxi/classic]
+---@license MIT
+
+---@class funsak.class
+local Object = {}
+Object.__index = Object
+
+--- IMPLEMENTATION REQUIRED: a constructor for a new instance of an object based
+--- on the class template that is extending this Object.
+---@vararg any[]? arguments to the constructor
+function Object:new(...) end
+
+--- extends this object definition to create a new class for a custom family
+--- of object instances
+---@return funsak.class extension extends the class.
+function Object:extend()
+  local cls = {}
+  for k, v in pairs(self) do
+    if k:find("__") == 1 then
+      cls[k] = v
+    end
+  end
+  cls.__index = cls
+  cls.super = self
+  setmetatable(cls, self)
+  return cls
+end
+
+--- attaches the target implementations of methods to the class
+---@vararg any[]?
+function Object:implement(...)
+  for _, cls in pairs({ ... }) do
+    for k, v in pairs(cls) do
+      if self[k] == nil and type(v) == "function" then
+        self[k] = v
+      end
+    end
+  end
+end
+
+--- checks to see if an object is equivalent to another object.
+---@param T any another object
+---@return boolean cond true if the object is equal to `T`, false otherwise.
+function Object:is(T)
+  local mt = getmetatable(self)
+  while mt do
+    if mt == T then
+      return true
+    end
+    mt = getmetatable(mt)
+  end
+  return false
+end
+
+--- IMPLEMENTATION REQUIRED: converts the given object into a string
+--- representation as a metamethod.
+---@return string rep the representation of the object as a string.
+function Object:__tostring()
+  return "Object"
+end
+
+--- treats the class definition like it might in Python, where the call to `new`
+--- can be tucked away and instantiation can occur when calling the class
+--- directly.
+---@vararg any[]?
+---@return funsak.class result the new instance.
+function Object:__call(...)
+  local obj = setmetatable({}, self)
+  obj:new(...)
+  return obj
+end
+
+return Object
