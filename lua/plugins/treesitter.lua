c137013fcce862717170fe69cd5050541b489bf2
Bailey Bjornstad <bailey@bjornstad.dev>
2023-11-21 10:32:28 -0700

    del(textsubjects): remove the treesitter-textsubjects plugin
    It seems as though this plugin has recently broken as a result of
    something related tangentially. I never use the textsubjects.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,6 @@
 local env = require("environment.ui")
+local key_view = require("environment.keys").view
+local key_treesitter = require("environment.keys").treesitter
 
 return {
   {
@@ -8,18 +10,8 @@ return {
       "nvim-treesitter/nvim-treesitter-textobjects",
       "nvim-treesitter/nvim-treesitter-context",
       "windwp/nvim-ts-autotag",
-      {
-        "andymass/vim-matchup",
-        config = function()
-          vim.g.matchup_matchparen_deferred = 1
-          vim.g.matchup_matchparen_offscreen = {
-            method = "status_manual",
-          }
-        end,
-      },
       "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/nvim-treesitter-refactor",
-      "RRethy/nvim-treesitter-textsubjects",
     },
     opts = {
       ensure_installed = "all",
@@ -27,17 +19,18 @@ return {
       indent = { enable = true },
       highlight = { enable = true },
       incremental_selection = {
-        enable = false,
+        enable = true,
         keymaps = {
-          init_selection = "<C-CR>",
-          node_incremental = "<C-CR>",
-          node_decremental = "<C-BS>",
+          init_selection = "gnn",
+          node_incremental = "gnn",
+          node_decremental = "gnm",
+          scope_incremental = "gnc",
         },
       },
       autotag = {
         enable = true,
         enable_rename = true,
-        enablescope_incremental_close = true,
+        enable_close = true,
         enable_close_on_slash = true,
       },
       endwise = { enable = true },
@@ -46,25 +39,35 @@ return {
         enable = true,
         enable_autocmd = false,
       },
-      textsubjects = {
-        enable = true,
-        prev_selection = "gpp", -- (Optional) keymap to select the previous selection
-        keymaps = {
-          ["gpN"] = "textsubjects-smart",
-          ["gpn"] = "textsubjects-container-outer",
-          ["gpi"] = "textsubjects-container-inner",
-        },
-      },
+      -- textsubjects = {
+      --   enable = true,
+      --   prev_selection = key_treesitter.modules.textsubjects.previous,
+      --   keymaps = {
+      --     [key_treesitter.modules.textsubjects.smart] = "textsubjects-smart",
+      --     [key_treesitter.modules.textsubjects.outer] = "textsubjects-container-outer",
+      --     [key_treesitter.modules.textsubjects.inner] = "textsubjects-container-inner",
+      --   },
+      -- },
       refactor = {
         highlight_definitions = {
           enable = true,
           -- Set to false if you have an `updatetime` of ~100.
-          clear_on_cursor_move = true,
+          clear_on_cursor_move = false,
         },
       },
     },
     build = ":TSUpdate",
-    -- get rid of the control space mapping overwrite here.
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    init = function()
+      vim.g.skip_ts_context_commentstring_module = true
+    end,
+    config = function(_, opts)
+      require("ts_context_commentstring").setup(opts)
+    end,
+    opts = {},
   },
   {
     "RRethy/nvim-treesitter-endwise",
@@ -90,15 +93,12 @@ return {
     opts = {
       enable = true,
       mode = "cursor",
-    }, -- separator = "🮩" },
+      zindex = 10,
+    },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
   {
     "windwp/nvim-ts-autotag",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-  },
-  {
-    "RRethy/nvim-treesitter-textsubjects",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
   {
@@ -120,11 +120,71 @@ return {
         { "[", "]" },
       },
       keymaps = {
-        init_selection = "<S-CR>",
-        node_incremental = "<S-CR>",
-        node_decremental = "<S-BS>",
+        init_selection = "<C-CR>",
+        node_incremental = "<C-CR>",
+        node_decremental = "<C-BS>",
       },
       filetype_exclude = env.ft_ignore_list,
     },
+  },
+  {
+    "haringsrob/nvim_context_vt",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+    config = true,
+    opts = {
+      enabled = true,
+      prefix = " 󰡱 󰟵 ",
+      disable_ft = env.ft_ignore_list,
+      min_rows = 0,
+      highlight = "NightowlContextHintsBright",
+    },
+    keys = {
+      {
+        key_view.context.toggle,
+        "<CMD>NvimContextVtToggle<CR>",
+        mode = "n",
+        desc = "context=> inline toggle",
+      },
+      {
+        key_view.context.debug,
+        "<CMD>NvimContextVtDebug<CR>",
+        mode = "n",
+        desc = "context=> inline debug",
+      },
+    },
+  },
+  {
+    "code-biscuits/nvim-biscuits",
+    enabled = false,
+    event = "VeryLazy",
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+      },
+    },
+    opts = {
+      cursor_line_only = true,
+      default_config = {
+        prefix_string = " 󰡱 󱛠 ",
+        max_length = 10,
+        min_distance = 3,
+        trim_by_words = true,
+      },
+      language_config = {
+        norg = {
+          disabled = true,
+        },
+        vimdoc = {
+          disabled = true,
+        },
+      },
+      toggle_keybind = key_view.context.biscuits,
+      on_events = { "InsertLeave", "CursorHoldI" },
+    },
+    config = function(_, opts)
+      require("nvim-biscuits").setup(opts)
+    end,
   },
 }
