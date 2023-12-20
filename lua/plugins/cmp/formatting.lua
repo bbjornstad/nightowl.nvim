cef69a6a11dc264bd9e6313b95887928030ec38d
Bailey Bjornstad <bailey@bjornstad.dev>
2023-12-20 02:26:42 -0700

    code(cmp): update implementations for nvim-cmp configs
    Namely related to sources, source parsing, and a bit of window
    configuration as well. Also a complete revamping of the mappings.

--- lua/plugins/cmp/formatting.lua
+++ lua/plugins/cmp/formatting.lua
@@ -4,7 +4,6 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      { "VonHeikemen/lsp-zero.nvim", optional = true },
       "onsails/lspkind.nvim",
     },
     opts = function(_, opts)
@@ -12,20 +11,62 @@ return {
       local has = require("lazyvim.util").has
       local cmp_format = require("lspkind").cmp_format({
         mode = "symbol_text",
-        preset = "codicons",
-        maxwidth = 60,
-        ellipsis_char = " ",
-        menu = {},
+        -- preset = "codicons",
+        maxwidth = 80,
+        menu = {
+          nvim_lsp = "[lsp]",
+          color_names = "[color]",
+          natdat = "[date]",
+          digraphs = "[digraph]",
+          gitlog = "[git]",
+          fonts = "[font]",
+          env = "[env]",
+          calc = "[calc]",
+          ctags = "[ctags]",
+          emoji = "[emoji]",
+          nvim_lsp_signature_help = "[signature]",
+          nvim_lua = "[lua]",
+          fuzzy_path = "[fpath]",
+          nvim_lsp_document_symbol = "[symbol]",
+          rg = "[rg]",
+          buffer = "[buf]",
+          luasnip = "[snip]",
+          look = "[look]",
+          ["diag-codes"] = "[diag]",
+          treesitter = "[tree]",
+          nerdfonts = "[nerd]",
+          nerdfont = "[nerd]",
+          AI = "[ai]",
+          spell = "[spell]",
+          ["cmp-dap"] = "[dap]",
+          omni = "[omni]",
+          latex_symbol = "[tex]",
+          ["cmp-tw2css"] = "[tw]",
+          pandoc_references = "[pandoc]",
+          zsh = "[zsh]",
+          cmdline = "[cmd]",
+          dictionary = "[dict]",
+          path = "[path]",
+          codeium = "[codeium]",
+          copilot = "[copilot]",
+          tabnine = "[tabnine]",
+          plugins = "[plugins]",
+        },
+        symbol_map = {
+          Codeium = "󱟬",
+          AI = "󱁊",
+          Copilot = "",
+        },
       })
       -- The following changes the appearance of the menu. Noted changes:
-      -- - different row field order
-      -- - vscode codicons
-      -- - vscode-styled colors
+      -- * different row field order
+      -- * vscode codicons
+      -- * vscode-styled colors
       opts.formatting = vim.tbl_deep_extend("force", {
         fields = {
+          cmp.ItemField.Menu,
           cmp.ItemField.Kind,
           cmp.ItemField.Abbr,
-          cmp.ItemField.Menu,
         },
         format = cmp_format,
       }, opts.formatting or {})
@@ -35,11 +76,13 @@ return {
       opts.window = vim.tbl_deep_extend("force", {
         completion = cmp.config.window.bordered({
           border = env.borders.main,
-          side_padding = 2,
+          scrolloff = 2,
+          side_padding = 1,
         }),
         documentation = cmp.config.window.bordered({
           border = env.borders.main,
-          side_padding = 2,
+          scrolloff = 4,
+          side_padding = 1,
         }),
       }, opts.window or {})
     end,
