d645ca7379546b9b4eab00dddd6e3c2168012f9f
Bailey Bjornstad <bailey@bjornstad.dev>
2023-12-20 02:15:25 -0700

    cfg(keys): update core lazyvim keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,7 @@
 local edit_tools = require("uutils.text")
 local kenv = require("environment.keys")
 local key_cline = kenv.editor.cline
+local key_lists = kenv.lists
 local mapx = vim.keymap.set
 local delx = vim.keymap.del
 local has = require("lazyvim.util").has
@@ -17,7 +18,7 @@ local function helpmapper()
   vim.cmd(thishelp)
 end
 
--- Rebind the help menu to be attached to "gh"
+-- Rebind the herp menu to be attached to "gh"
 mapx(
   "n",
   "gH",
@@ -84,14 +85,16 @@ delx({ "n" }, "<leader>l")
 -- I don't want to.
 mapx({ "n", "v" }, "<S-h>", "_", { desc = "::goto=> line first character" })
 mapx({ "n", "v" }, "<S-l>", "$", { desc = "::goto=> line end character" })
-mapx("i", "<A-S-h>", "<C-o>_", { desc = "::goto=> line first character" })
-mapx("i", "<A-S-l>", "<C-o>$", { desc = "::goto=> line end character" })
+mapx("i", "<C-S-h>", "<C-o>_", { desc = "::goto=> line first character" })
+mapx("i", "<C-S-l>", "<C-o>$", { desc = "::goto=> line end character" })
+mapx("i", "<C-j>", "<C-o><down>", { desc = "::buf=> cursor down" })
+mapx("i", "<C-k>", "<C-o><up>", { desc = "::buf=> cursor up" })
+mapx("i", "<C-h>", "<C-o><left>", { desc = "::buf=> cursor left" })
+mapx("i", "<C-l>", "<C-o><right>", { desc = "::buf=> cursor right" })
 
 if not has("cybu.nvim") then
   mapx("n", "<C-S-h>", "<cmd>bprevious<cr>", { desc = "::buf=> previous" })
   mapx("n", "<C-S-l>", "<cmd>bnext<cr>", { desc = "::buf=> next" })
-  mapx("i", "<C-S-h>", "<C-o><cmd>bprevious<cr>", { desc = "::buf=> previous" })
-  mapx("i", "<C-S-l>", "<C-o><cmd>bnext<cr>", { desc = "::buf=> next" })
   mapx("n", "[b", "<cmd>bprevious<cr>", { desc = "::buf=> previous" })
   mapx("n", "]b", "<cmd>bnext<cr>", { desc = "::buf=> next" })
 end
@@ -113,37 +116,55 @@ mapx("n", "<C-q>!", "<CMD>quit!<CR>", { desc = "::quit=> [!] terminate" })
 mapx("n", "<C-q><C-!>", "<CMD>quit!<CR>", { desc = "::quit=> [!] terminate" })
 mapx(
   "n",
-  "<C-Q>w",
+  "<C-S-q>w",
   "<CMD>wqall<CR>",
   { desc = "::quit=> [all] save > terminate" }
 )
 mapx(
   "n",
-  "<C-Q><C-w>",
+  "<C-S-q><C-w>",
   "<CMD>wqall<CR>",
   { desc = "::quit=> [all] save > terminate" }
 )
-mapx("n", "<C-Q>Q", "<CMD>quitall<CR>", { desc = "::quit=> [all] terminate" })
+mapx("n", "<C-S-q>Q", "<CMD>quitall<CR>", { desc = "::quit=> [all] terminate" })
 mapx(
   "n",
-  "<C-Q><C-Q>",
+  "<C-S-q><C-S-q>",
   "<CMD>quitall<CR>",
   { desc = "::quit=> [all] terminate" }
 )
 mapx(
   "n",
-  "<C-Q><C-!>",
+  "<C-S-q><C-!>",
   "<CMD>quitall!<CR>",
   { desc = "::quit=> [all, !] terminate" }
 )
 mapx(
   "n",
-  "<C-Q>!",
+  "<C-S-q>!",
   "<CMD>quitall!<CR>",
   { desc = "::quit=> [all, !] terminate" }
 )
 
+-- remove macro on the q key, as I prefer to use a plugin to handle macros and
+-- the q key is prime real estate for other functions.
+-- At one point macros were probably crucially important to the vim experience,
+-- but I think that language tooling has evolved substantially enough to where
+-- this is less of a showstopper of a feature.
+mapx("n", "q", "<NOP>", { desc = "macro=> don't record" })
+
+-- upgrades to closing behavior.
+mapx("n", "<C-c>", function()
+  vim.api.nvim_win_close(0, false)
+end, { desc = "::win=> close window" })
+mapx("n", "qqq", function()
+  vim.api.nvim_win_close(0, false)
+end, { desc = "::win=> close window" })
+
+-- remove some default binds to free them up for different entries under the "f"
+-- subleader
 delx("n", "<leader>fn")
+delx("n", "<leader>ft")
 mapx("n", kenv.fm.fs.new, function()
   vim.ui.input({ prompt = "enter filename:" }, function(input)
     vim.cmd(("edit ./%s"):format(input))
@@ -184,14 +205,97 @@ delx({ "n" }, "<leader>bb")
 delx({ "n" }, "<leader>qq")
 
 -- things to move windows around
-mapx("n", "<A-h>", "<C-w>h", { desc = "::win=> go left", remap = true })
-mapx("n", "<A-j>", "<C-w>j", { desc = "::win=> go down", remap = true })
-mapx("n", "<A-k>", "<C-w>k", { desc = "::win=> go up", remap = true })
-mapx("n", "<A-l>", "<C-w>l", { desc = "::win=> go right", remap = true })
-mapx("n", "<leader>wh", "<C-w>h", { desc = "::win=> go left", remap = false })
-mapx("n", "<leader>wj", "<C-w>j", { desc = "::win=> go down", remap = false })
-mapx("n", "<leader>wk", "<C-w>k", { desc = "::win=> go up", remap = false })
-mapx("n", "<leader>wl", "<C-w>l", { desc = "::win=> go right", remap = false })
+mapx("n", "<A-h>", "<C-w>h", { desc = "::win.go=> left", remap = true })
+mapx("n", "<A-j>", "<C-w>j", { desc = "::win.go=> down", remap = true })
+mapx("n", "<A-k>", "<C-w>k", { desc = "::win.go=> up", remap = true })
+mapx("n", "<A-l>", "<C-w>l", { desc = "::win.go=> right", remap = true })
+mapx("n", "<leader>wh", "<C-w>h", { desc = "::win.go=> left", remap = false })
+mapx("n", "<leader>wj", "<C-w>j", { desc = "::win.go=> down", remap = false })
+mapx("n", "<leader>wk", "<C-w>k", { desc = "::win.go=> go up", remap = false })
+mapx("n", "<leader>wl", "<C-w>l", { desc = "::win.go=> right", remap = false })
+mapx("n", "<leader>wH", "<C-w>H", { desc = "::win.mv=> left", remap = false })
+mapx("n", "<leader>wJ", "<C-w>J", { desc = "::win.mv=> bottom", remap = false })
+mapx("n", "<leader>wK", "<C-w>K", { desc = "::win.mv=> top", remap = false })
+mapx("n", "<leader>wL", "<C-w>L", { desc = "::win.mv=> right", remap = false })
+mapx(
+  "n",
+  "<leader>wr",
+  "<C-w>r",
+  { desc = "::win.rot=> rotate down/right", remap = false }
+)
+mapx(
+  "n",
+  "<leader>wR",
+  "<C-w>R",
+  { desc = "::win.rot=> rotate up/left", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w<",
+  "<C-w><",
+  { desc = "::win.sz.width=> decrease", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w>",
+  "<C-w>r",
+  { desc = "::win.sz.width=> increase", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w-",
+  "<C-w>-",
+  { desc = "::win.sz.height=> decrease", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w+",
+  "<C-w>+",
+  { desc = "::win.sz.height=> increase", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w|",
+  "<C-w>|",
+  { desc = "::win.sz.width=> maximize", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w_",
+  "<C-w>|",
+  { desc = "::win.sz.height=> maximize", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w=",
+  "<C-w>=",
+  { desc = "::win.sz=> maximize equally", remap = false }
+)
+mapx(
+  "n",
+  "<leader>wo",
+  "<C-w>o",
+  { desc = "::win.close=> all other windows", remap = false }
+)
+mapx(
+  "n",
+  "<leader>wT",
+  "<C-w>T",
+  { desc = "::win.tab=> to new", remap = false }
+)
+mapx(
+  "n",
+  "<leader>w<tab>",
+  "<C-w>T",
+  { desc = "::win.tab=> to new", remap = false }
+)
+
+mapx("n", key_lists.quickfix.open, "<CMD>copen<CR>", { desc = "qf=> open" })
+mapx("n", key_lists.quickfix.next, "<CMD>cnext<CR>", { desc = "qf=> open" })
+mapx("n", key_lists.quickfix.last, "<CMD>clast<CR>", { desc = "qf=> open" })
+mapx("n", key_lists.quickfix.previous, "<CMD>cprev<CR>", { desc = "qf=> open" })
+mapx("n", key_lists.quickfix.first, "<CMD>cfirst<CR>", { desc = "qf=> open" })
+mapx("n", key_lists.quickfix.close, "<CMD>cclose<CR>", { desc = "qf=> open" })
 
 -- Some basic line breaks using comment characters.
 -- There are more advanced text-generation and commented breakline methods
@@ -221,27 +325,23 @@ mapx("n", key_cline.custom.following.nospace, function()
   edit_tools.SucceedingCommentBreak(false)
 end, { desc = "::break.following=> insert (no-space)" })
 
-if has("accelerated-jk.nvim") then
-  delx({ "n", "v" }, "j")
-  delx({ "n", "v" }, "k")
-  mapx({ "n", "v" }, "j", "<Plug>(accelerated_jk_j)", { desc = "mtn=> down" })
-  mapx({ "n", "v" }, "k", "<Plug>(accelerated_jk_k)", { desc = "mtn=> up" })
-end
-
 -- these mappings are somewhat annoying as by default the alt-j key and alt-k
 -- keys act to move the line up or down another line (e.g. swap line with above
 -- or below line.) It is too easy for me to accidentally hit this when I don't
 -- mean to.
 delx({ "n", "v", "i" }, "<A-j>")
 delx({ "n", "v", "i" }, "<A-k>")
-mapx({ "n", "v" }, "<A-l>", "<Right>", { desc = "mtn=> right" })
-mapx({ "n", "v" }, "<A-h>", "<Left>", { desc = "mtn=> left" })
-mapx({ "n", "v" }, "<A-k>", "<Up>", { desc = "mtn=> up" })
-mapx({ "n", "v" }, "<A-j>", "<Down>", { desc = "mtn=> down" })
-mapx({ "i" }, "<A-l>", "<C-o><Right>", { desc = "mtn=> right" })
-mapx({ "i" }, "<A-h>", "<C-o><Left>", { desc = "mtn=> left" })
-mapx({ "i" }, "<A-k>", "<C-o><Up>", { desc = "mtn=> up" })
-mapx({ "i" }, "<A-j>", "<C-o><Down>", { desc = "mtn=> down" })
+
+-- helpers for quitting when accidentally using Shift
+vim.cmd("cnoreabbrev W! w!")
+vim.cmd("cnoreabbrev Q! q!")
+vim.cmd("cnoreabbrev Qall! qall!")
+vim.cmd("cnoreabbrev Wq wq")
+vim.cmd("cnoreabbrev Wa wa")
+vim.cmd("cnoreabbrev wQ wq")
+vim.cmd("cnoreabbrev WQ wq")
+vim.cmd("cnoreabbrev W w")
+vim.cmd("cnoreabbrev Q q")
 
 -- binding to toggle centering the cursor when scrolling up or down.
 mapx({ "n" }, "<leader>uR", function()
