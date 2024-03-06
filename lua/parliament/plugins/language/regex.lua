1c52d06fe26c6d361373b6dacf22244d3fc001dc
Bailey Bjornstad <bailey@bjornstad.dev>
2024-03-05 19:56:15 -0700

    fix(regex): remove overlapping regex explainer plugin
    this plugin is also created by someone who is clearly so brainwashed by
    propaganda that they feel the need to turn their neovim plugin into a political
    sounding board for a genocide

--- lua/parliament/plugins/language/regex.lua
+++ lua/parliament/plugins/language/regex.lua
@@ -3,60 +3,6 @@ local key_regex = require("environment.keys").tool.regex
 
 return {
   {
-    "bennypowers/nvim-regexplainer",
-    config = true,
-    ft = {
-      "html",
-      "js",
-      "cjs",
-      "mjs",
-      "ts",
-      "jsx",
-      "tsx",
-      "cjsx",
-      "mjsx",
-    },
-    cmd = {
-      "Regexplainer",
-      "RegexplainerShowSplit",
-      "RegexplainerShowPopup",
-      "RegexplainerToggle",
-    },
-    opts = {
-      mode = "narrative",
-      display = "popup",
-      auto = true,
-      filetypes = {
-        "html",
-        "js",
-        "cjs",
-        "mjs",
-        "ts",
-        "jsx",
-        "tsx",
-        "cjsx",
-        "mjsx",
-      },
-      popup = {
-        border = {
-          style = env.borders.main,
-          padding = { 1, 2 },
-        },
-      },
-    },
-    keys = {
-      {
-        key_regex.explainer,
-        "<CMD>RegexplainerToggle<CR>",
-        mode = "n",
-        desc = "regex=> explain",
-      },
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-  },
-  {
     "tomiis4/hypersonic.nvim",
     config = true,
     ft = {
