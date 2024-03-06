f4e45ebec8552ba924cbf0fb7a52f2dc9635c0a3
Bailey Bjornstad <bailey@bjornstad.dev>
2024-03-06 10:55:18 -0700

    refactor(ts): introduce supporting ts extensions
    This includes a refactoring of the way that extension config options are passed
    to treesitter; now they can be passed directly on the LazySpec corresponding to
    the plugin providing the external extension.

--- lua/parliament/plugins/treesitter.lua
+++ lua/parliament/plugins/treesitter.lua
@@ -1,6 +1,21 @@
 local env = require("environment.ui")
-local key_view = require("environment.keys").view
-local key_replace = require("environment.keys").replace
+local kenv = require("environment.keys")
+local lz = require("funsak.lazy")
+local key_view = kenv.view
+local key_replace = kenv.replace
+local key_treesj = kenv.tool.splitjoin
+local function openURL(url)
+  local opener
+  if vim.fn.has("macunix") == 1 then
+    opener = "open"
+  elseif vim.fn.has("linux") == 1 then
+    opener = "xdg-open"
+  elseif vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+    opener = "start"
+  end
+  local openCommand = string.format("%s '%s' >/dev/null 2>&1", opener, url)
+  vim.fn.system(openCommand)
+end
 
 return {
   {
@@ -14,65 +29,47 @@ return {
       { "JoosepAlviste/nvim-ts-context-commentstring", optional = true },
       { "nvim-treesitter/nvim-treesitter-refactor", optional = true },
     },
-    opts = {
-      ensure_installed = "all",
-      ignore_install = { "comment" },
-      auto_install = true,
-      indent = { enable = true },
-      highlight = { enable = true },
-      incremental_selection = {
+    opts = function(_, opts)
+      opts.ensure_installed = "all"
+      opts.ignore_install =
+        vim.list_extend({ "comment" }, opts.ignore_install or {})
+      opts.auto_install = true
+      opts.indent =
+        vim.tbl_deep_extend("force", { enable = true }, opts.indent or {})
+      opts.highlight =
+        vim.tbl_deep_extend("force", { enable = true }, opts.highlight or {})
+      opts.incremental_selection = vim.tbl_deep_extend("force", {
         enable = false,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "gnn",
-          node_decremental = "gnm",
-          scope_incremental = "gnc",
-        },
-      },
-      -- endwise = { enable = true },
-      matchup = { enable = true },
-      playground = { enable = true },
-      query_linter = { enable = true },
-      refactor = {
-        highlight_current_scope = {
-          enable = false,
-        },
-        highlight_definitions = {
-          enable = false,
-        },
-        smart_rename = {
-          enable = true,
-          keymaps = {
-            smart_rename = key_replace.treesitter,
-          },
-        },
-        navigation = {
-          enable = true,
-          keymaps = {
-            goto_definition = key_view.treesitter_nav.definition,
-            goto_next_usage = key_view.treesitter_nav.next_usage,
-            goto_previous_usage = key_view.treesitter_nav.previous_usage,
-            list_definitions = key_view.treesitter_nav.list_definitions,
-            list_definitions_toc = key_view.treesitter_nav.list_definitions_toc,
-          },
-        },
-      },
-      textobjects = {
-        lsp_interop = {
-          enable = true,
-          border = env.borders.main,
-        },
-        move = {
-          enable = true,
-        },
-        select = {
-          enable = true,
-        },
-        swap = {
-          enable = false,
-        },
-      },
-    },
+      }, opts.incremental_selection or {})
+      -- matchup = { enable = true },
+      opts.query_linter =
+        vim.tbl_deep_extend("force", { enable = true }, opts.query_linter or {})
+      opts.refactor = vim.tbl_deep_extend(
+        "force",
+        lz.opts("nvim-treesitter-refactor"),
+        opts.refactor or {}
+      )
+      opts.endwise = vim.tbl_deep_extend(
+        "force",
+        lz.opts("nvim-treesitter-endwise"),
+        opts.endwise or {}
+      )
+      opts.textobjects = vim.tbl_deep_extend(
+        "force",
+        lz.opts("nvim-treesitter-textobjects"),
+        opts.textobjects or {}
+      )
+      opts.textsubjects = vim.tbl_deep_extend(
+        "force",
+        lz.opts("nvim-treesitter-textsubjects"),
+        opts.textsubjects or {}
+      )
+      opts.autotag = vim.tbl_deep_extend(
+        "force",
+        lz.opts("nvim-ts-autotag"),
+        opts.autotag or {}
+      )
+    end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
     end,
@@ -86,16 +83,16 @@ return {
       vim.g.skip_ts_context_commentstring_module = true
     end,
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ matchup = { enable = true } })
       require("ts_context_commentstring").setup(opts)
     end,
+    opts = { enable_autocmd = true },
   },
   {
     "RRethy/nvim-treesitter-endwise",
     ft = { "ruby", "lua", "vim", "bash", "elixir", "fish", "julia" },
     opts = { enable = true },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ endwise = opts })
+      -- require("nvim-treesitter.configs").setup({ endwise = opts })
     end,
   },
   {
@@ -103,7 +100,7 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = "VeryLazy",
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ textsubjects = opts })
+      -- require("nvim-treesitter.configs").setup({ textsubjects = opts })
     end,
     opts = {
       enable = true,
@@ -123,9 +120,23 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = "VeryLazy",
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ textobjects = opts })
+      -- require("nvim-treesitter.configs").setup({ textobjects = opts })
     end,
-    opts = {},
+    opts = {
+      lsp_interop = {
+        enable = true,
+        border = env.borders.main,
+      },
+      move = {
+        enable = true,
+      },
+      select = {
+        enable = true,
+      },
+      swap = {
+        enable = false,
+      },
+    },
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
@@ -146,6 +157,9 @@ return {
       mode = "cursor",
       zindex = 10,
     },
+    config = function(_, opts)
+      require("treesitter-context").setup(opts)
+    end,
   },
   {
     "windwp/nvim-ts-autotag",
@@ -157,7 +171,7 @@ return {
       enable_close_on_slash = true,
     },
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ autotag = opts })
+      -- require("nvim-treesitter.configs").setup({ autotag = opts })
     end,
   },
   {
@@ -165,13 +179,30 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = "VeryLazy",
     config = function(_, opts)
-      require("nvim-treesitter.configs").setup({ refactor = opts })
+      -- require("nvim-treesitter.configs").setup({ refactor = opts })
     end,
     opts = {
+      highlight_current_scope = {
+        enable = false,
+      },
       highlight_definitions = {
+        enable = false,
+      },
+      smart_rename = {
         enable = true,
-        -- Set to false if you have an `updatetime` of ~100.
-        clear_on_cursor_move = true,
+        keymaps = {
+          smart_rename = key_replace.treesitter,
+        },
+      },
+      navigation = {
+        enable = true,
+        keymaps = {
+          goto_definition = key_view.treesitter_nav.definition,
+          goto_next_usage = key_view.treesitter_nav.next_usage,
+          goto_previous_usage = key_view.treesitter_nav.previous_usage,
+          list_definitions = key_view.treesitter_nav.list_definitions,
+          list_definitions_toc = key_view.treesitter_nav.list_definitions_toc,
+        },
       },
     },
   },
@@ -190,9 +221,9 @@ return {
         { "[", "]" },
       },
       keymaps = {
-        init_selection = "<C-CR>",
-        node_incremental = "<C-CR>",
-        node_decremental = "<C-BS>",
+        init_selection = "<BS>",
+        node_incremental = "<BS>",
+        node_decremental = "<S-BS>",
       },
       filetype_exclude = env.ft_ignore_list,
     },
@@ -259,5 +290,154 @@ return {
     config = function(_, opts)
       require("nvim-biscuits").setup(opts)
     end,
+  },
+  {
+    "jdrupal-dev/code-refactor.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = {
+      {
+        "<leader>cR",
+        "<cmd>CodeActions all<CR>",
+        mode = "n",
+      },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("code-refactor").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    event = "VeryLazy",
+    opts = {},
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+    end,
+  },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_outer()
+        end,
+        mode = { "x", "o" },
+        desc = "ts:obj| indent |=> outer",
+      },
+      {
+        "aI",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_outer(true)
+        end,
+        mode = { "x", "o" },
+        desc = "ts:obj| indent |=> outer [line]",
+      },
+      {
+        "ii",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_inner()
+        end,
+        mode = { "x", "o" },
+        desc = "ts:obj| indent |=> inner",
+      },
+      {
+        "iI",
+        function()
+          require("treesitter_indent_object.textobj").select_indent_inner(
+            true,
+            "V"
+          )
+        end,
+        mode = { "x", "o" },
+        desc = "ts:obj| indent |=> inner [line]",
+      },
+    },
+  },
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    opts = {
+      useDefaultKeymaps = true,
+      disabledKeymaps = { "ii", "ai", "iI", "aI", "gw", "gW", "gc", "gC" },
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("various-textobjs").setup(opts)
+    end,
+    keys = {
+      {
+        "gx",
+        function()
+          require("various-textobjs").url()
+          local found = vim.fn.mode():find("v")
+          if not found then
+            return
+          end
+          -- retrieve URL with the z-register as intermediary
+          vim.cmd.normal({ "\"zy", bang = true })
+          local url = vim.fn.getreg("z")
+          openURL(url)
+        end,
+        mode = "n",
+        desc = "ts:obj| gxnext |=> open link",
+      },
+    },
+  },
+  {
+    "Wansmer/treesj",
+    keys = {
+      {
+        key_treesj.toggle,
+        function()
+          require("treesj").toggle()
+        end,
+        mode = "n",
+        desc = "treesj=> toggle fancy splitjoin",
+      },
+      {
+        key_treesj.join,
+        function()
+          require("treesj").join()
+        end,
+        mode = "n",
+        desc = "treesj=> join with splitjoin",
+      },
+      {
+        key_treesj.split,
+        function()
+          require("treesj").split()
+        end,
+        mode = "n",
+        desc = "treesj=> split with splitjoin",
+      },
+    },
+    config = function(_, opts)
+      require("treesj").setup(opts)
+    end,
+    opts = {
+      -- Use default keymaps
+      -- (<space>m - toggle, <space>j - join, <space>s - split)
+      use_default_keymaps = false,
+
+      -- Node with syntax error will not be formatted
+      check_syntax_error = true,
+
+      -- If line after join will be longer than max value,
+      -- node will not be formatted
+      max_join_length = 120,
+
+      -- hold|start|end:
+      -- hold - cursor follows the node/place on which it was called
+      -- start - cursor jumps to the first symbol of the node being formatted
+      -- end - cursor jumps to the last symbol of the node being formatted
+      cursor_behavior = "hold",
+
+      -- Notify about possible problems or not
+      notify = true,
+
+      -- Use `dot` for repeat action
+      dot_repeat = true,
+    },
   },
 }
