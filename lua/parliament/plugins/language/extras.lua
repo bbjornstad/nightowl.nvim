65158547957031e53c079bd84c40fdb1e9006f19
Bailey Bjornstad <bailey@bjornstad.dev>
2024-03-28 01:31:33 -0600

    update(rest): reconfigure after new rest.nvim major version
    This release namely uses luarocks for setup; there is a new schema for
    configuration that must be followed. This commit adjusts accordingly.

--- lua/parliament/plugins/language/extras.lua
+++ lua/parliament/plugins/language/extras.lua
@@ -11,72 +11,112 @@ return {
   },
   {
     "rest-nvim/rest.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = "http",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "vhyrro/luarocks.nvim",
+    },
     opts = {
-      -- Open request results in a horizontal split
-      result_split_horizontal = false,
-      -- Keep the http file buffer above|left when split horizontal|vertical
-      result_split_in_place = false,
-      -- Skip SSL verification, useful for unknown certificates
-      skip_ssl_verification = false,
-      -- Encode URL before making request
+      client = "curl",
+      env_file = ".env",
+      env_pattern = "\\.env$",
+      env_edit_command = "tabedit",
       encode_url = true,
-      -- Highlight request on run
-      highlight = {
-        enabled = true,
-        timeout = 150,
+      skip_ssl_verification = false,
+      custom_dynamic_variables = {},
+      logs = {
+        level = "info",
+        save = true,
       },
       result = {
-        -- toggle showing URL, HTTP info, headers at top the of result window
-        show_url = true,
-        -- show the generated curl command in case you want to launch
-        -- the same request via the terminal (can be verbose)
-        show_curl_command = true,
-        show_http_info = true,
-        show_headers = true,
-        -- executables or functions for formatting response body [optional]
-        -- set them to false if you want to disable them
-        formatters = {
-          json = "jq",
-          html = function(body)
-            return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-          end,
+        split = {
+          horizontal = false,
+          in_place = false,
+          stay_in_current_window_after_split = true,
+        },
+        behavior = {
+          decode_url = true,
+          show_info = {
+            url = true,
+            headers = true,
+            http_info = true,
+            curl_command = true,
+          },
+          statistics = {
+            enable = true,
+            ---@see https://curl.se/libcurl/c/curl_easy_getinfo.html
+            stats = {
+              { "total_time", title = "Time taken:" },
+              { "size_download_t", title = "Download size:" },
+            },
+          },
+          formatters = {
+            json = "jq",
+            html = function(body)
+              if vim.fn.executable("tidy") == 0 then
+                return body, { found = false, name = "tidy" }
+              end
+              local fmt_body = vim.fn
+                .system({
+                  "tidy",
+                  "-i",
+                  "-q",
+                  "--tidy-mark",
+                  "no",
+                  "--show-body-only",
+                  "auto",
+                  "--show-errors",
+                  "0",
+                  "--show-warnings",
+                  "0",
+                  "-",
+                }, body)
+                :gsub("\n$", "")
+
+              return fmt_body, { found = true, name = "tidy" }
+            end,
+          },
         },
       },
-      -- Jump to request line on run
-      jump_to_request = false,
-      env_file = ".env",
-      custom_dynamic_variables = {},
-      yank_dry_run = true,
+      highlight = {
+        enable = true,
+        timeout = 750,
+      },
+      ---Example:
+      ---
+      ---```lua
+      ---keybinds = {
+      ---  {
+      ---    "<localleader>rr", "<cmd>Rest run<cr>", "Run request under the cursor",
+      ---  },
+      ---  {
+      ---    "<localleader>rl", "<cmd>Rest run last<cr>", "Re-run latest request",
+      ---  },
+      ---}
+      ---
+      ---```
+      ---@see vim.keymap.set
+      keybinds = {
+        {
+          key_rest.open,
+          "<cmd>Rest run<CR>",
+          "rest:| client |=> open",
+        },
+        {
+          key_rest.last,
+          "<CMD>Rest run last<CR>",
+          "rest:| last |=> open",
+        },
+        {
+          key_rest.log,
+          "<CMD>Rest log<CR>",
+          "rest:| log |=> open",
+        },
+      },
     },
-    config = true,
-    cmd = { "RestNvim", "RestNvimPreview", "RestNvimLast" },
-    keys = {
-      {
-        key_rest.open,
-        "<Plug>RestNvim",
-        mode = "n",
-        desc = "rest:| client |=> open",
-      },
-      {
-        key_rest.preview,
-        "<Plug>RestNvimPreview",
-        mode = "n",
-        desc = "rest:| preview |=> open",
-      },
-      {
-        key_rest.last,
-        "<Plug>RestNvimLast",
-        mode = "n",
-        desc = "rest:| last |=> open",
-      },
-      {
-        key_rest.log,
-        "<CMD>RestLog<CR>",
-        mode = "n",
-        desc = "rest:| log |=> open",
-      },
-    },
+    config = function(_, opts)
+      require("rest-nvim").setup(opts)
+    end,
   },
   {
     "https://git.sr.ht/~reggie/licenses.nvim",
